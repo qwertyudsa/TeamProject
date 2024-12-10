671462a7db908f030dccb21ce3f3cfbacd396917
irina-big <anutka-a94@yandex.ru>
2024-12-10 19:39:53 +0300

    allure reports has been added

--- src/test/java/irinabigtests/AlexTests.java
+++ src/test/java/irinabigtests/AlexTests.java
@@ -3,6 +3,7 @@ package irinabigtests;
 import allclasses.irinabig.Alex;
 import allclasses.irinabig.Feline;
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,6 +14,7 @@ public class AlexTests {
     @Test
     @DisplayName("Проверка того, что друзья Алекса - это Мартин, Глория, Мелман")
     @Description("Тестовый метод первый о том, что у Алекса есть друзья!")
+    @Step("Шаг первый")
     public void testMethodOne() throws Exception {
         Feline  feline = new Feline();
         Alex alex = new Alex(feline);
@@ -21,6 +23,7 @@ public class AlexTests {
     @Test
     @DisplayName("Проверка того, что у Алекса нет котят")
     @Description("Тестовый метод второй о том, что у Алекса нет котят!")
+    @Step("Шаг второй")
     public void testMethodTwo() throws Exception {
         Feline  feline = new Feline();
         Alex alex = new Alex(feline);
