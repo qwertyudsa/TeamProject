213fdf393bfd6ca1e015b8ad177a7a34e225f537
aimoure <annaa5879@gmail.com>
2024-12-11 13:11:32 +0300

    изменение pom.xml

--- src/test/java/yakovleva/DeerTests.java
+++ src/test/java/yakovleva/DeerTests.java
@@ -9,12 +9,21 @@ import java.time.Period;
 import org.junit.Assert;
 import org.junit.Test;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.junit4.DisplayName; // импорт DisplayName
+
 public class DeerTests 
 {
     @Test
+    @DisplayName("Проверка метода подсчета возраста оленя день рождения которого до сегодняшнего дня")
+    @Description("Проверка метода подсчета возраста оленя день рождения которого до сегодняшнего дня (12.01.2020)")
+    @TmsLink("Test-Case 101")
+    @Issue("BUG-404")
     public void AgeDayBeforeToday3() 
     {
-        Deer deer = new Deer("Ми", LocalDate.of(2020, 10, 12), Type.лань);
+        Deer deer = new Deer("Ми", LocalDate.of(2020, 1, 12), Type.лань);
        
         int actual = deer.Age();
 
@@ -25,6 +34,10 @@ public class DeerTests
     }
 
     @Test
+    @DisplayName("Проверка метода подсчета возраста оленя день рождения которого позже сегодняшнего дня")
+    @Description("Проверка метода подсчета возраста оленя день рождения которого позже сегодняшнего дня (28.12.2020)")
+    @TmsLink("Test-Case 102")
+    @Issue("BUG-404")
     public void AgeDayAfterToday2() 
     {
         Deer deer = new Deer("Ми", LocalDate.of(2020, 12, 28), Type.лань);
@@ -38,6 +51,10 @@ public class DeerTests
     }
 
     @Test
+    @DisplayName("Проверка метода подсчета возраста оленя день рождения которого еще не наступил")
+    @Description("Проверка метода подсчета возраста оленя день рождения которого еще не наступил в этом году (12.12.2026)")
+    @TmsLink("Test-Case 103")
+    @Issue("BUG-404")
     public void AgeDateAfterToday0() 
     {
         Deer deer = new Deer("Ми", LocalDate.of(2026, 12, 12), Type.лань);
