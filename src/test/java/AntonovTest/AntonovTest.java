a358e3bdeba29782a798f94a3ad7d2372d281a57
Artem <portovartem2006@gmail.com>
2024-12-19 16:51:16 +0300

    Merge remote-tracking branch 'origin/portov' into portov

--- src/test/java/AntonovTest/AntonovTest.java
+++ src/test/java/AntonovTest/AntonovTest.java
@@ -12,7 +12,7 @@ public class AntonovTest {
     @Test
     public void testInitialValues() {
         Bear bear = new Bear("Antonov", 0, 0);
-        assertEquals(null, bear.rank, "Начальный ранг должен быть Herald");
+        assertEquals(null, bear.rank);// "Начальный ранг должен быть Herald");
         assertEquals(0, bear.pts);//, "Начальный уровень PTS должен быть 1");
         assertEquals(0, bear.numberOfGames);//, "Начальное количество игр должно быть 0");
         assertEquals(0, bear.hours);//, "Начальное количество часов должно быть 0");
@@ -41,10 +41,10 @@ public class AntonovTest {
         Bear bear = new Bear("Antonov", 0, 0);
         bear.pts = 700; // Устанавливаем PTS в диапазон Guardian
         bear.yourRank();
-        assertEquals("Guardian", bear.rank, "Ранг должен быть Guardian при PTS 700");
+        assertEquals("Guardian", bear.rank);// "Ранг должен быть Guardian при PTS 700");
 
         bear.pts = 2200; // Устанавливаем PTS в диапазон Archon
         bear.yourRank();
-        assertEquals("Archon", bear.rank, "Ранг должен быть Archon при PTS 2200");
+        assertEquals("Archon", bear.rank);// "Ранг должен быть Archon при PTS 2200");
     }
 }
