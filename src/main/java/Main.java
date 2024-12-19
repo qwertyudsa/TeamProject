5122b9314941dfaae55143b09fa9f73fa9957097
Sofya Chernyshova <sof4ern@mail.ru>
2024-12-19 14:19:11 +0300

    Add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        // Демонстрация работы классов пакета irinabig
+        //Демонстрация работы классов пакета irinabig
         try {
             showClassesIrinaBig();
             showClassesTovstogan();
