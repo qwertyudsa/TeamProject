b922788f349d4aacf5c8f54912329062233c7464
paulinochka <mfpolina2006@gmail.com>
2024-12-26 17:02:54 +0300

    Update kittens.java

--- src/main/java/allclasses/petukhova/kittens.java
+++ src/main/java/allclasses/petukhova/kittens.java
@@ -1,5 +1,11 @@
 package allclasses.petukhova;
 
 public class kittens {
+  String catMom = "Kitten's mom";
+  String catDad = "Kitten's dad";
+  String kitten = "This is a kitten";
+  void printInfo() {
+    System.out.printf("This is: %s \n This is: %s \n And: %s", catMom, catDad, kitten);
+  }
 
 }
