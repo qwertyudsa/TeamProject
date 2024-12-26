70f8e8110e22a0628ced0aa2d9b3690a81f8f4a6
paulinochka <mfpolina2006@gmail.com>
2024-12-26 17:33:35 +0300

    Update kittenstest.java

--- src/test/java/petukhovatests/kittenstest.java
+++ src/test/java/petukhovatests/kittenstest.java
@@ -1,4 +1,13 @@
 package petukhovatests;
 
+import allclasses.petukhova.kittens;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class kittenstest {
+    @Test
+    public bool testGetcorrectStringReturnTrue(){
+        kittens kitten = new kittens();
+        Assert.assertEquals(kitten.PrintInfo(), "This is: Kitten's mom \n This is: Kitten's dad \n And: This is a kitten");
+    }
 }
